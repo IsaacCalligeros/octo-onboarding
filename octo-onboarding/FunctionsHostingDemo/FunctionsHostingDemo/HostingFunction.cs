8f9269f8dfd97fe108ef4446b60e5fc40585e36f
isaac <isaac.calligeros@gmail.com>
2022-03-15 13:59:00 +1030

    adding static files need to update proxies

--- octo-onboarding/FunctionsHostingDemo/FunctionsHostingDemo/HostingFunction.cs
+++ octo-onboarding/FunctionsHostingDemo/FunctionsHostingDemo/HostingFunction.cs
@@ -5,6 +5,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using System;
 
 namespace HostingFunctionsApp
 {
@@ -25,42 +27,41 @@ namespace HostingFunctionsApp
         // Serves static files for client UI
         [FunctionName("ui")]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ui/{p1?}/{p2?}/{p3?}")] HttpRequest req,
-            string p1,
-            string p2,
-            string p3,
-            ExecutionContext context
-        )
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ui/{p1?}/{p2?}/{p3?}")] HttpRequest req, ExecutionContext context)
         {
             var TrimString = "octo-onboarding";
             string functionRoot = context.FunctionAppDirectory;
             var root = functionRoot.Substring(0, functionRoot.LastIndexOf(TrimString) + 1 + TrimString.Length);
-
-            // Sanitizing input, just in case
-            string path = $"{Path.GetFileName(p1)}";
-            path += p2 != null ? $"/{Path.GetFileName(p2)}" : "";
-            path += p3 != null ? $"/{Path.GetFileName(p3)}" : "";
-
-            var contentType = FileMap.FirstOrDefault((kv => path.StartsWith(kv[0])));
-            if (contentType != null)
-            {
-                return File.Exists(root + path) ?
-                    (IActionResult)new FileStreamResult(File.OpenRead(root + path), contentType[1]) :
-                    new NotFoundResult();
-            }
-            // Returning index.html by default, to support client routing
-            var test = File.ReadAllText($"{root}/build/index.html");
             return new FileStreamResult(File.OpenRead($"{root}/build/index.html"), "text/html; charset=UTF-8");
         }
 
         private static readonly string[][] FileMap = new string[][]
         {
-        new [] { "build/static/css/", "text/css; charset=utf-8"},
-        new [] { "build/static/media/", "image/svg+xml; charset=UTF-8"},
-        new [] { "build/static/js/", "application/javascript; charset=UTF-8"},
-        new [] { "build/manifest.json", "application/json; charset=UTF-8"},
-        new [] { "build/service-worker.js", "application/javascript; charset=UTF-8"},
-        new [] { "build/favicon.ico", "image/x-icon"}
+            new [] { "static/css/", "text/css; charset=utf-8"},
+            new [] { "static/media/", "image/svg+xml; charset=UTF-8"},
+            new [] { "static/js/", "application/javascript; charset=UTF-8"},
+            new [] { "manifest.json", "application/json; charset=UTF-8"},
+            new [] { "service-worker.js", "application/javascript; charset=UTF-8"},
+            new [] { "favicon.ico", "image/x-icon"}
         };
+
+
+        [FunctionName("ServeStaticFile")]
+        public static IActionResult ServeStaticFile(
+          [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ExecutionContext context)
+        {
+            var TrimString = "octo-onboarding";
+            string functionRoot = context.FunctionAppDirectory;
+            var root = functionRoot.Substring(0, functionRoot.LastIndexOf(TrimString) + 1 + TrimString.Length);
+            var file = req.Query["file"].ToString();
+
+            var contentType = FileMap.FirstOrDefault((kv => file.StartsWith(kv[0])));
+
+            var filePath = $"{root}/build/{file}";
+
+            return File.Exists(filePath) ?
+                (IActionResult)new FileStreamResult(File.OpenRead(filePath), contentType[1]) :
+                new NotFoundResult();
+        }
     }
 }
